d2bebc95702b2a758c9dcb17754f697e480f0ac3
Paulo Belo <pjbelo@gmail.com>
2017-12-22 16:37:28 +0000

    v0.30

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 //import a library to help create component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 //Create a component
 
 const App = () => (
-    <Header headerText={'Albums'} />
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 // Render it to the device
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { View, Text} from 'react-native';
 
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>Album list!!!</Text>
+        </View>
+    );
+};
+
+export default AlbumList;
